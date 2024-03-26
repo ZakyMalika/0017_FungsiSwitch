2cfa12481a15ff6a10abdc35dcdc5f4cc6007a86
ZakyMalika <zakymalika0987@gmail.com>
2024-03-26 17:44:25 +0700

    membuat output di int main

--- pemdas/pemdas.cpp
+++ pemdas/pemdas.cpp
@@ -26,6 +26,11 @@ int pembagian(float a, float b) {
 
 int main() {
 	do {
-		cout <<"Ka "
+		cout << "Kalkulator Sederhana " << endl;
+		cout << "====================" << endl;
+		cout << "1. Penjumlahan" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Perkalian" << endl;
+		cout << "4. pembagian" << endl;
 	} while
 }
