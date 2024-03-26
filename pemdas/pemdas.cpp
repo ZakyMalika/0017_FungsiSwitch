6707d225f4d5bca89082492d515094c465a2aa26
ZakyMalika <zakymalika0987@gmail.com>
2024-03-26 17:38:02 +0700

    menambahkan variable global

--- pemdas/pemdas.cpp
+++ pemdas/pemdas.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+int bilA, bilB;
+
+void input() {
+	cout << "Masukkan bilangan pertama = ";
+	cin >> bilA;
+	cout << "Masukkan bilangan kedua = ";
+	cin >> bilB;
+}
