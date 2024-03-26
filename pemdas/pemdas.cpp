8515f1dfd993564ad19e2d7620ee0458a175366c
ZakyMalika <zakymalika0987@gmail.com>
2024-03-26 17:47:03 +0700

    menambahkan output kelayar

--- pemdas/pemdas.cpp
+++ pemdas/pemdas.cpp
@@ -25,12 +25,21 @@ int pembagian(float a, float b) {
 }
 
 int main() {
+	int pilihan;
 	do {
 		cout << "Kalkulator Sederhana " << endl;
 		cout << "====================" << endl;
 		cout << "1. Penjumlahan" << endl;
 		cout << "2. Pengurangan" << endl;
 		cout << "3. Perkalian" << endl;
-		cout << "4. pembagian" << endl;
+		cout << "4. Pembagian" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "====================" << endl;
+		cout << "Masukkan Menu Pilihan : " << endl;
+		cin >> pilihan;
+
+		switch (pilihan) {
+
+		}
 	} while
 }
